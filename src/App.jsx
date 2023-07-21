9b28d996aa62b5b7d5d61a2b8dbb61054af87af5
busayolawumi <busayoolawumi3@gmail.com>
2023-07-21 14:03:23 +0100

    Added search functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Home from "./pages/Home";
 import Category from "./components/Category";
 import Pages from "./pages/Pages";
 import { BrowserRouter } from "react-router-dom";
+import Search from "./components/Search";
 
 const App = () => {
 	return (
 		<>
 			<BrowserRouter>
+				<Search />
 				<Category />
 				<Pages />
 			</BrowserRouter>

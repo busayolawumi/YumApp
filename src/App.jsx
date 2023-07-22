bd9ead8a80db115206d6bd39220b8e40503a0b70
busayolawumi <busayoolawumi3@gmail.com>
2023-07-22 13:57:09 +0100

    Add Navbar and Logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import Home from "./pages/Home";
 import Category from "./components/Category";
 import Pages from "./pages/Pages";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 import Search from "./components/Search";
+import { styled } from "styled-components";
+import { GiKnifeFork } from "react-icons/gi";
 
 const App = () => {
 	return (
 		<>
 			<BrowserRouter>
+				<Nav>
+					<Logo to={"/"}>
+						<GiKnifeFork />
+						Yumm App
+					</Logo>
+				</Nav>
 				<Search />
 				<Category />
 				<Pages />
@@ -16,5 +24,22 @@ const App = () => {
 		</>
 	);
 };
+
+const Logo = styled(Link)`
+	text-decoration: none;
+	font-size: 1.5rem;
+	font-weight: 400;
+	font-family: "Lobster Two", cursive;
+`;
+
+const Nav = styled.div`
+	padding: 4rem 0rem;
+	display: flex;
+	justify-content: flex-start;
+	align-items: center;
+	svg {
+		font-size: 2rem;
+	}
+`;
 
 export default App;
